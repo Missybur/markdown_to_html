1f1f5dbfc03dac689ec60336874196446dea5a43
Missy Bur <missybur@gmail.com>
2015-11-10 11:34:31 -0800

    Add changes to port

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+// "use strict";
+
+// var PORT = process.env.PORT || 3000;
+
+// var express = require("express");
+// var morgan = require('morgan')
+// var bodyParser = require('body-parser');
+
+// var marked = require('marked');
+
+// var app = express();
+
+// app.set('view engine', 'jade');
+
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+
+// app.get("/", function(req, res){
+//   res.render("index");
+// });
+
+// app.get("/markdown", function(req, res){
+//   res.render("markdown");
+// })
+
+// app.post("/markdown", function(req, res){
+//   res.send(marked(req.body.md));
+// });
+
+// app.listen(PORT, function(){
+//   console.log("listening on port %s",PORT)
+// });
+
+
+
 "use strict";
 
 var express = require("express");
@@ -22,5 +59,5 @@ app.post("/", function(req, res){
   res.send(html);
 });
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
